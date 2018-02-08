34174d75f24521e753ed5b037f921422b0168832
Kallu609 <khirvola@gmail.com>
2018-02-08 15:13:39 +0200

    Finished product

--- app.js
+++ app.js
@@ -1,37 +1,71 @@
-const config = require('./config.js');
-const url = require('url');
-const request = require('request-promise');
-const requestImageSize = require('request-image-size');
+const config    = require('./config.js');
+const fs        = require('fs');
+const path      = require('path');
+const ratio     = require('aspect-ratio');
+const request   = require('request-promise');
+const shortid   = require('shortid');
 const wallpaper = require('wallpaper');
 
 function pickRandom(array) {
-    return array[Math.floor(Math.random() * array.length)];
+  return array[Math.floor(Math.random() * array.length)];
 }
 
 function buildUrl() {
-    return `https://www.reddit.com/r/${pickRandom(config.subreddits)}/${config.sort}.json?t=${config.from}&limit=${config.limit}`;
+  return `https://www.reddit.com/r/${pickRandom(config.subreddits)}/${config.sort}.json?t=${config.from}&limit=${config.limit}`;
 }
 
-const redditUrl = buildUrl();
+function download(uri, filename, callback) {
+  request.head(uri, (err, res, body) => {
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+}
 
-request(redditUrl)
-    .then((htmlString) => {
-        const json = JSON.parse(htmlString);
+function changeWallpaper() {
+  request(buildUrl())
+  .then((htmlString) => {
+    const json = JSON.parse(htmlString);
 
-        let urls = json.data.children
-            .map(submission => submission.data.url)
-            .filter(submission => {
-                const host = url.parse(submission).host;
-                return config.domains.includes(host);
-            });
+    let images = json.data.children
+      .map(submission => {
+        const width  = submission.data.preview.images[0].source.width;
+        const height = submission.data.preview.images[0].source.height;
 
-    })
-    .catch((err) => {
-        throw err;
+        if (submission.data.post_hint === 'image' && width >= config.minSize.width && height >= config.minSize.height && config.aspectRatios.includes(ratio(width, height))) {
+          return {
+            url:       submission.data.url,
+            ext:       submission.data.url.split('.').pop(),
+            domain:    submission.data.domain,
+            subreddit: submission.data.subreddit,
+            width:     width,
+            height:    height,
+          };
+        }
+      })
+      .filter(submission => {
+        if (submission) {
+          return config.domains.includes(submission.domain);
+        }
+      });
+
+    // No images found from that subreddit, let's try again.
+    if (images.length === 0) {
+      changeWallpaper();
+      return;
+    }
+
+    const randomImage = pickRandom(images);
+    const targetPath = path.join('wallpapers', (shortid.generate() + '.' + randomImage.ext));
+      
+    download(randomImage.url, targetPath, () => {
+      wallpaper.set(targetPath, { scale: config.scale }).then(() => {
+        console.log(`Wallpaper set to '${targetPath}' from subreddit '/r/${randomImage.subreddit}'`)
+      });
     });
-    
-/*
-wallpaper.set('wallpaper.png').then(() => {
-	console.log('done');
-});
-*/
+  })
+  .catch((err) => {
+    throw err;
+  });
+}
+
+changeWallpaper();
+setInterval(changeWallpaper, config.interval * 1000 * 60);
--- config.js
+++ config.js
@@ -1,21 +1,37 @@
 const config = {
-    // List of allowed domains for wallpapers
-    "domains": ["i.imgur.com", "imgur.com", "i.redd.it"],
+  // List of allowed domains for wallpapers
+  domains: ['i.imgur.com', 'imgur.com', 'i.redd.it'],
 
-    // List of allowed file-types
-    "types": ["png", "jpg", "jpeg"],
-    
-    // List of subreddits to get wallpapers from
-    "subreddits": ["earthporn", "wallpapers"],
+  // List of allowed file-types
+  types: ['png', 'jpg', 'jpeg'],
 
-    // Sort
-    "sort": "top",
+  // List of subreddits to get wallpapers from
+  // 'subreddits': ['wallpapers'],
+  subreddits: ['EarthPorn', 'CityPorn', 'SkyPorn', 'WeatherPorn', 'BotanicalPorn', 'LakePorn', 'VillagePorn', 'BeachPorn', 'WaterPorn', 'SpacePorn', 'multiwall', 'wallpapers', 'wallpaper'],
 
-    // Timeframe of posts to get
-    "from": "year",
+  // Sort
+  sort: 'top',
 
-    // Limit of posts to get (Your wallpaper will be chosen randomly)
-    "limit": 15
+  // Timeframe of posts to get
+  from: 'year',
+
+  // Limit of posts to get (Your wallpaper will be chosen randomly)
+  limit: 15,
+
+  // Minimum size of image
+  minSize: {
+    height: 1080,
+    width: 0
+  },
+  
+  // Allowed aspect ratios
+  aspectRatios: ['16:9'],
+
+  // Scaling method for the wallpaper. Options: fill, fit, stretch, center
+  scale: 'fill',
+
+  // How often to change wallpaper (in minutes)
+  interval: 60, // Every hour
 }
 
 module.exports = config;
